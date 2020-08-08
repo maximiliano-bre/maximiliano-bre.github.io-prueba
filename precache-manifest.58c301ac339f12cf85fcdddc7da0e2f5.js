90011b8c3455995179208d5e0b37e24cea84d470
maximiliano-bre <69249706+maximiliano-bre@users.noreply.github.com>
2020-08-07 21:46:35 -0300

    ss

---
+++ precache-manifest.58c301ac339f12cf85fcdddc7da0e2f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b47e7bc8368f44feb5362793f98963a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bc63d3366fc47959be6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "13976f00493ca691647c",
+    "url": "/static/js/2.1947ded5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1947ded5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bc63d3366fc47959be6",
+    "url": "/static/js/main.21bfe9e4.chunk.js"
+  },
+  {
+    "revision": "e69856d8b72ca632e24c",
+    "url": "/static/js/runtime-main.5413961d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ precache-manifest.ba36ab79e4d8cf83e220f28a1a015875.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27112b3c1243c4f14c909096f3278899",
+    "url": "/prueba/index.html"
+  },
+  {
+    "revision": "555a9b068bc407c0a6fa",
+    "url": "/prueba/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "13976f00493ca691647c",
+    "url": "/prueba/static/js/2.1947ded5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/prueba/static/js/2.1947ded5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "555a9b068bc407c0a6fa",
+    "url": "/prueba/static/js/main.03c5a178.chunk.js"
+  },
+  {
+    "revision": "70ba8ab3b6491fafac0d",
+    "url": "/prueba/static/js/runtime-main.e93451a0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/prueba/static/media/logo.5d5d9eef.svg"
+  }
+]);
